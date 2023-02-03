a516801503d954a24f574785b9f151ceecb03326
Yu Kuratake <yukurata@outlook.com>
2023-02-04 00:22:33 +0900

    Add i18n language switching support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { NextIntlProvider } from 'next-intl';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // If you don't use internationalized routing, you need to pass a `locale` to the provider.
+  return (
+    <NextIntlProvider messages={pageProps.messages}>
+      <Component {...pageProps} />
+    </NextIntlProvider>
+  );
 }
