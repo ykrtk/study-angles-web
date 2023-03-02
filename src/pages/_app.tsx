9ffadc72705850f7a83dc683ecc59577e0d060d0
Yu Kuratake <yukurata@outlook.com>
2023-03-03 01:32:57 +0900

    Add Analytics to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
-import { AngleProvider } from '@/components/providers/AngleProvider';
 import '@/styles/globals.scss';
 import { NextIntlProvider } from 'next-intl';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+import { AngleProvider } from '@/components/providers/AngleProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
     // If you don't use internationalized routing, you need to pass a `locale` to the provider.
     return (
-        <NextIntlProvider messages={pageProps.messages}>
-            <AngleProvider>
-                <Component {...pageProps} />
-            </AngleProvider>
-        </NextIntlProvider>
+        <>
+            <NextIntlProvider messages={pageProps.messages}>
+                <AngleProvider>
+                    <Component {...pageProps} />
+                </AngleProvider>
+            </NextIntlProvider>
+            <Analytics />
+        </>
     );
 }
